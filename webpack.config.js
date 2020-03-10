b22be230f0a3e165758c89b0070266e57d6fccbc
22earth <zhifengle@gmail.com>
2020-03-10 13:17:28 +0800

    chore: use typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,36 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const webpack = require('webpack');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    // Each entry in here would declare a file that needs to be transpiled
-    // and included in the extension source.
-    // For example, you could add a background script like:
     // background: './src/background.js',
-    popup: './src/popup.js',
-    content: './src/content/index.js',
-    bangumi: './src/content/bangumi.js',
-    background: './src/bg/index.js'
+    // popup: './src/popup.js',
+    // content: './src/content/index.js',
+    // bangumi: './src/content/bangumi.js',
+    // background: './src/bg/index.js'
+    test: './src/index.ts'
   },
   output: {
-    // This copies each source entry into the extension dist folder named
-    // after its entry config key.
     path: path.join(__dirname, 'extension/dist'),
     filename: '[name].js',
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
-        test: /.less|.css$/,
-        use: ExtractTextPlugin.extract([
-          { loader: 'css-loader', options: { minimize: true }}, "less-loader"]
-        )
-        // ["style-loader", "css-loader", "less-loader"]
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-    ]
+      // { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
   },
   plugins: [
-    // Since some NodeJS modules expect to be running in Node, it is helpful
-    // to set this environment var to avoid reference errors.
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new ExtractTextPlugin('[name].css')
   ],
-  devtool: 'sourcemap',
+  devtool: 'inline-source-map',
+  // devtool: 'cheap-module-source-map',
+  mode: 'production'
 };
