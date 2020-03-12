d81aa892e54d81faea7d805ade9722c4dc8500a6
22earth <zhifengle@gmail.com>
2020-03-12 22:57:53 +0800

    update: dom utils

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -1,4 +1,17 @@
 /**
+ * 为页面添加样式
+ * @param style
+ */
+export const addStyle = (style: string) => {
+  const styleTag = document.createElement('style');
+  styleTag.innerHTML = style;
+  document.head.appendChild(styleTag);
+};
+
+function getText(elem: HTMLElement): string {
+  return (elem.textContent || elem.innerText || '')
+}
+/**
  * dollar 选择单个
  * @param {string} selector
  */
@@ -24,3 +37,25 @@ export function injectScript(fn: Function, data: Object) {
   script.innerHTML = `(${fn.toString()})(${data});`;
   document.body.appendChild(script);
 }
+
+/**
+ * 查找包含文本的标签
+ * @param {string} selector
+ * @param {string} text
+ */
+export function contains(selector: string, text: string | string[], $parent: HTMLElement) {
+  let elements;
+  if ($parent) {
+    elements = $parent.querySelectorAll(selector);
+  } else {
+    elements = $qa(selector);
+  }
+  let t = text as any;
+  if (Array.isArray(t)) {
+    t = t.join('|');
+  }
+  return [].filter.call(elements, function (element: HTMLElement) {
+    return new RegExp(t).test(getText(element));
+  });
+}
+
