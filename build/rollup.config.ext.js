208db4928d80a2fdcad7876e5b29bbfb11cf9056
22earth <zhifengle@gmail.com>
2020-03-16 13:26:08 +0800

    chore: build extension

---
+++ build/rollup.config.ext.js
@@ -0,0 +1,46 @@
+import {resolve} from 'path'
+import base from './rollup.config.base'
+
+const bangumi = {
+  input: resolve(__dirname, '../src/content/bangumi.ts'),
+  output: {
+    file: resolve(__dirname, '../extension/dist/bangumi.js')
+  }
+}
+const bg = {
+  input: resolve(__dirname, '../src/bg/index.ts'),
+  output: {
+    file: resolve(__dirname, '../extension/dist/background.js')
+  }
+}
+const popup = {
+  input: resolve(__dirname, '../src/popup.ts'),
+  output: {
+    file: resolve(__dirname, '../extension/dist/popup.js')
+  }
+}
+const config = [
+  {
+    input: resolve(__dirname, '../src/content/index.ts'),
+    output: {
+      file: resolve(__dirname, '../extension/dist/content.js')
+    }
+  }
+]
+
+export default [
+  {
+    ...base,
+    ...bangumi,
+  },
+  {
+    ...base,
+    ...bg,
+  },
+  {
+    ...base,
+    ...popup,
+  },
+  ...config.map(obj => ({...base, ...obj}))
+]
+
---
+++ src/content/bangumi.ts
@@ -0,0 +1,4 @@
+function init() {
+  console.log('hello')
+}
+init();
