bcf5bcb0b082719146249f920df7cda6746163dc
22earth <zhifengle@gmail.com>
2020-03-17 23:47:32 +0800

    fix: deal date

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import {genRandomStr} from "./utils";
+import {dealDate, genRandomStr} from "./utils";
 
 describe('test utils', () => {
   it('test the length of return value', () => {
@@ -6,5 +6,11 @@ describe('test utils', () => {
   })
   it('return value', () => {
     expect(genRandomStr(5)).toMatch(/^[a-zA-Z0-9]{5}$/)
+  })
+  test('deal date', () => {
+    expect(dealDate('2019年2月19')).toEqual('2019-02-19')
+    expect(dealDate('2019年12月')).toEqual('2019-12')
+    expect(dealDate('2019/2/19')).toEqual('2019-02-19')
+    expect(dealDate('2019/2')).toEqual('2019-02')
   })
 })
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,11 +11,33 @@ export function randomNum(max: number, min: number): number {
 }
 
 export function dealDate(dataStr: string): string {
-  let l = dataStr.split('/');
+  // 2019年12月19
+  let l: string[] = [];
+  if (/\d{4}年\d{1,2}月(\d{1,2}日?)?/.test(dataStr)) {
+    l = dataStr.replace('日', '').split(/年|月/).filter(i => i);
+  } else if (/\d{4}\/\d{1,2}(\/\d{1,2})?/.test(dataStr)) {
+    l = dataStr.split('/');
+  } else if (/\d{4}-\d{1,2}(-\d{1,2})?/.test(dataStr)) {
+    return dataStr
+  } else {
+    throw new Error('invalid date str')
+  }
   return l.map((i) => {
     if (i.length === 1) {
       return `0${i}`;
     }
     return i;
   }).join('-');
+}
+
+export function isEqualDate(d1: string, d2: string): boolean {
+  const resultDate = new Date(d1)
+  const originDate = new Date(d2)
+  if (resultDate.getFullYear() === originDate.getFullYear() &&
+    resultDate.getMonth() === originDate.getMonth() &&
+    resultDate.getDate() === originDate.getDate()
+  ) {
+    return true;
+  }
+  return false;
 }
